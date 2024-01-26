b8bd3e3c70fe1064ba4420301611b8cbc5996856
Gemma <gemma.garrigosa@gmail.com>
2024-01-26 16:42:42 +0100

    Corregit exercici10

--- exercici10.py
+++ exercici10.py
@@ -6,10 +6,14 @@ intents = 0
 
 while True:
     numero = int(input('Introdueix un numero'))
-    if escollit > numero:
-        print(f'El nombre és més gran que {numero}')
-    elif escollit < numero:
-        print(f'El nombre és més petit que {numero}')
+
+    if numero <= 100 and numero >= 1:
+        if escollit > numero:
+            print(f'El nombre és més gran que {numero}')
+        elif escollit < numero:
+            print(f'El nombre és més petit que {numero}')
+        else:
+            print(f'HAS ENCERTAT :D !!')
+            break
     else:
-        print(f'HAS ENCERTAT :D !!')
-        break
+        print("Introdueix un nombre entre 0 i 100 siusplau")
