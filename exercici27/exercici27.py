08b6ecc24fcf579f367f8fdade7f3954c9a0dd0c
Gemma <gemma.garrigosa@gmail.com>
2024-02-02 16:24:33 +0100

    Afegit exercici 27

---
+++ exercici27/exercici27.py
@@ -0,0 +1,2 @@
+def suma_nombres(num1,num2):
+    return num1 + num2
---
+++ exercici27/main.py
@@ -0,0 +1,8 @@
+from exercici27 import suma_nombres
+
+num1 = int(input('Introdueix un número'))
+num2 = int(input('Introdueix un altre número'))
+
+resultat = suma_nombres(num1,num2)
+
+print(resultat)
