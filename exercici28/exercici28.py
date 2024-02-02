2d1b9db5e9a39a91a692210f972971082a34f660
Gemma <gemma.garrigosa@gmail.com>
2024-02-02 16:42:34 +0100

    Afegit exercici 28

---
+++ exercici28/exercici28.py
@@ -0,0 +1,4 @@
+
+import random
+def numero_random(num1,num2):
+    return random.randint(num1,num2)
---
+++ exercici28/main.py
@@ -0,0 +1,8 @@
+from exercici28 import numero_random
+
+num1 = int(input('Introdueix un nombre'))
+num2 = int(input('Introdueix un altre nombre'))
+
+resultat = numero_random(num1,num2)
+
+print(resultat)
