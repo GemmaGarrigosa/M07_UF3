b59d088ac6228da19a53fd202961c446bb94ad1d
Gemma <gemma.garrigosa@gmail.com>
2024-02-02 16:51:01 +0100

    Afegit exercici 29

---
+++ exercici29/exercici29.py
@@ -0,0 +1,5 @@
+def suma_rang(num1,num2):
+    llista_nums = list(range(num1,num2 +1))
+
+    return (f'Llista de nombres: {llista_nums} \n'
+            f'Suma dels nombres: {sum(llista_nums)}')
---
+++ exercici29/main.py
@@ -0,0 +1,6 @@
+from exercici29 import suma_rang
+
+num1 = int(input('Introdueix un nombre: '))
+num2 = int(input('Introdueix un altre nombre: '))
+
+print(suma_rang(num1,num2))
