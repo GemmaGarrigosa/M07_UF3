2340923ab01123c5caaf0665a9aecdee32b30aa7
Gemma <gemma.garrigosa@gmail.com>
2024-02-07 18:32:45 +0100

    modificat create_table

--- PRACTICA2/create_table.py
+++ PRACTICA2/create_table.py
@@ -1,21 +1,43 @@
+import psycopg2
+
 from connection import *
+resposta = input('Vols connectar amb la base de dades? (si/no)')
 
-#Creem la taula de pokemons a la BD
-# id, nom , tipus1, tipus2, pes, mida
+if resposta == 'si':
+    try:
+            conn = psycopg2.connect(
+                database="practica2",
+                user="gemma",
+                password="system",
+                host="localhost",
+                port="5432"
+            )
 
-sql = '''CREATE TABLE POKEMON(
-                pokemon_id SERIAL PRIMARY KEY,
-                pokemon_name VARCHAR(255) NOT NULL,
-                pokemon_type1 VARCHAR(255) NOT NULL,
-                pokemon_type2 VARCHAR(255),
-                pokemon_weight FLOAT NOT NULL,
-                pokemon_height FLOAT NOT NULL               
-)'''
+            connection = conn.cursor()
+            #Creem la taula de pokemons a la BD
 
-print(sql)
 
-#Enviar query
-connection.execute(sql)
+            sql = '''CREATE TABLE POKEMON(
+                            pokemon_id SERIAL PRIMARY KEY,
+                            pokemon_name VARCHAR(255) NOT NULL,
+                            pokemon_type1 VARCHAR(255) NOT NULL,
+                            pokemon_type2 VARCHAR(255),
+                            pokemon_weight FLOAT NOT NULL,
+                            pokemon_height FLOAT NOT NULL               
+            )'''
 
-#Commit per a fer els canvis
-conn.commit()
+            print(sql)
+
+            #Enviar query
+            connection.execute(sql)
+
+            #Commit per a fer els canvis
+            conn.commit()
+
+    except (Exception,psycopg2.Error) as error:
+        print("Error", error)
+    finally:
+        conn.close()
+
+else:
+    print('No has volgut crear la taula')
