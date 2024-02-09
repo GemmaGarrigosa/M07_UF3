e2b879f4236b1bdf37531439718e2de86a697132
Gemma <gemma.garrigosa@gmail.com>
2024-02-09 16:30:42 +0100

    modificat main

--- PRACTICA2/Main.py
+++ PRACTICA2/Main.py
@@ -1,7 +1,10 @@
 import create_table
 import connection
 import psycopg2
-
+import create
+import read
+import update
+import delete
 #Programa empieza, connecta bbdd, pregunta si quieres crear la tabla, switch de metodes
 try:
     response = input('Vols crear la taula? (si/no)')
@@ -9,19 +12,29 @@ try:
     if response == 'si':
         create_table.crear_taula()
 
-    action = input(',Llegir(R)'
-                   ',Actualitzar(U)'
-                   ',Eliminar(D)').upper()
+    while True:
+        action = input('Crear(C)'
+                       ',Llegir(R)'
+                       ',Actualitzar(U)'
+                       ',Eliminar(D),'
+                       'Sortir(0)').upper()
 
 
-    if action == 'R':
-        print('metode read')
-    elif action == 'U':
-        print('metode update')
-    elif action == 'D':
-        print('metode delete')
-    else:
-        print('Susplau introdueix una acció vàlida')
+        if action =='R':
+            read.llegir_taula()
+
+        elif action == 'U':
+            update.actualitza_pokemon()
+
+        elif action == 'D':
+            delete.eliminar_pokemon()
+
+        elif action == 'C':
+            create.crear_pokemon()
+
+        elif action == '0':
+            print('bye :D ')
+            break
 
 
 except (Exception, psycopg2.Error) as error:
